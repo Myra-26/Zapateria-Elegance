16a28e7674a5ec5fc8a866162af63499dd8a1513
Myra-26 <77389990@certus.edu.pe>
2026-08-27 21:10:55 -0500

    Implement login form submission and validation
    Add login functionality with username and password validation.

--- ap.js
+++ ap.js
@@ -1,1 +1,15 @@
-
+document.getElementById('login-container').addEventListener('submit', function(event) { 
+event.preventDefault(); 
+const username = document.getElementById('username').value; 
+const password = document.getElementById('password').value; 
+// usuario y la contraseñas 
+const validUsername = 'Admisaurio'; 
+const validPassword = '1234'; 
+if (username === validUsername && password === validPassword) { 
+// rederigir a la pagina de la tienda 
+window.location.href = 'tienda.html'; 
+} else { 
+// mensaje de error
+document.getElementById('errorMessge').textContent = 'Invalid username or password'; 
+} 
+}); 
